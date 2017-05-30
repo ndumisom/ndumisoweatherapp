b791a5f1d8a7784e8bd4290090de051d1e3f277e
ndumiso <nmbete@tagworldwide.com>
2017-05-30 08:57:00 +0200

    Display the temperatures that don’t appear in the combined high and low dataset.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-weather api showing live data
+LiveWeather
 ===========
--- js/main.js
+++ js/main.js
@@ -27,8 +27,7 @@ var date = new Date();
     			   BV.show(['night.mp4'],{ambient:true});
                    });
  }
-
-$('.submit').click(function(){
+ $('.submit').click(function(){
 	//Define the text field that will take city name to the weather api
 	var city = $('.city_names').val();
 	$.ajax({
@@ -64,58 +63,90 @@ $('.submit').click(function(){
                      "<td style='color: #fff;padding-right: 20%;width: 50%;'>Date & time</td>"+
 	    	 		"<td style='color: #fff;padding-right: 0%;width: 20%;'>Max</td>"+
 	    	 		"<td style='color: #fff;padding-right: 0%;width: 20%;'>Min</td>"+
-                    "<td style='color: #fff;padding-right: 0%;padding-left: 18%;width: 50%;'>Description</td>"+
+                    "<td style='color: #fff;padding-right: %;width: 22%;'>Description</td>"+
 	    	 		"</tr>");
 
 	    	 	   var tbobdy = [];
 	    	 	   	tbobdy.push();
 	    	 	   	for (var i = 0; i < response.list.length; i++) {
                     tbobdy.push("<tr>"+
-	    	 		"<td style='color: #fff;padding-right: 0%;width: 39%;'>"+ response.list[i].dt_txt +"</td>"+
-	    	 		"<td style='color: #fff;padding-right: 0%;width: 20%;'>"+ response.list[i].main.temp_max +"</td>"+
-	    	 		"<td style='color: #fff;padding-right: 0%;width: 20%;'>"+ response.list[i].main.temp_min +"</td>"+
+	    	 		"<td style='color: #fff;padding-right: 0%;width: 40%;'>"+ response.list[i].dt_txt +"</td>"+
+	    	 		"<td style='color: #fff;padding-right: 0%;width: 15%;'>"+ response.list[i].main.temp_max +"</td>"+
+	    	 		"<td style='color: #fff;padding-right: 0%;width: 15%;'>"+ response.list[i].main.temp_min +"</td>"+
                     
-                    "<td style='color: #fff;padding-right: %;width: 39%;'>"+ response.list[i].weather[0].description +"</td>"+
+                    "<td style='color: #fff;padding-right: %;width: 35%;'>"+ response.list[i].weather[0].description +"</td>"+
 	    	 		"</tr>");
-	    	 		
-	    	 	}
-
-	    	 	
-	    	 		tableBody = tbobdy[0];
+                }
+	    	 	tableBody = tbobdy[0];
                    for (var i = 1; i < tbobdy.length; i++) {
                       tableBody = tableBody+tbobdy[i];
                    }
-	    	 		
-
-	    	 		$(".table-body").html(tableBody);
-
-	    	 		console.log(document.getElementsByClassName("tbody")[0]);
-                   
-
-                   var calculateAverageHighTem = 0;
+                   $("tbody").html(tableBody);
+                    var calculateAverageHighTem = 0;
                    for (var i = 0; i < response.list.length; i++) {
-
-                      calculateAverageHighTem = calculateAverageHighTem+response.list[i].main.temp_max;
+                   	calculateAverageHighTem = calculateAverageHighTem+response.list[i].main.temp_max;
                    }
-
-                    var calculateAverageLowTem = 0;
+                   var calculateAverageLowTem = 0;
                    for (var i = 0; i < response.list.length; i++) {
 
                       calculateAverageLowTem = calculateAverageLowTem+response.list[i].main.temp_min;
                    }
-	   	    		
-	   	    		AverageHighTemperature = calculateAverageHighTem/response.list.length;
-
-	   	    		calculateAverageLowTem = calculateAverageLowTem/response.list.length;
+                   AverageHighTemperature = calculateAverageHighTem/response.list.length;
+                   calculateAverageLowTem = calculateAverageLowTem/response.list.length;
 	   	    	    $('.average-high-temperature').html("<tr>"+
                      "<td style='color: #fff;padding-right: 0%;width: 50%;'>Average High&Low Temperature</td>"+
 	    	 		"<td style='color: #fff;padding-right: 0%;width: 20%;'>"+AverageHighTemperature.toFixed(2)+"</td>"+
 	    	 		"<td style='color: #fff;padding-left: 10%;width: 20%;'>"+calculateAverageLowTem.toFixed(2)+"</td>"+
                     "<td style='color: #fff;padding-right: 0%;padding-left: 0%;width: 0%;'></td>"+
 	    	 		"</tr>");
-                    temperaturesNotOnHighLowdataset(response);
+	    	 
+                    var temNotOnLonHigh = temperaturesNotOnHighLowdataset(response);
+
+                     sumOf = "<p>Temp not in the data set</p>";
+
+                     for (var i = 0; i < temNotOnLonHigh.length; i++) {
+                     	sumOf = sumOf+ "<p>"+temNotOnLonHigh[i]+"</p>";
                      }
-	});
+
+			          if(temNotOnLonHigh.length>0){
+			           $('.missing-data-set').html(sumOf);
+			          }
+			          else {
+			          	$('.missing-data-set').html(sumOf+"<p>None</p>");
+			          }
+
+                   var createDictionalHighLow = createDictionalHighLowTemperatures(response);
+
+                   dictionarySum = "<p>Dictinary</p>";
+
+                   for (var key in createDictionalHighLow) {
+                   if (createDictionalHighLow.hasOwnProperty(key)) {
+                       dictionarySum=dictionarySum+"<p>"+(key + " -> " + createDictionalHighLow[key])+"</p>";
+						  }
+						}
+
+			         
+			           $('.dictinary').html(dictionarySum);
+			       
+			  
+
+                    displayOveral = displayOverallMinMax(response);
+
+                    displayOveralSum = "<p>display Overall Min Max</p>";
+
+                     for (var i = 0; i < displayOveral.length; i++) {
+                     	displayOveralSum = displayOveralSum + "<p>"+displayOveral[i]+"</p>";
+                     }
+
+			          if(displayOveral.length>0){
+			           $('.overall-min-max').html(displayOveralSum);
+			          }
+			          else {
+			          	$('.overall-min-max').html(displayOveralSum+"<p>None</p>");
+			          }
+             
+	}
+});
 });
 
 $(document).ready(function(){
@@ -167,15 +198,27 @@ $(document).ready(function(){
 	
 });
 
-
-function temperaturesNotOnHighLowdataset(response) {
-        var findSmallestTemperature = [];
+function temperaturesNotOnHighLowdataset(response){
+	    var findSmallestTemperature = [];
+	    var findLargetTemperature = [];
         for (var k = 0; k < response.list.length; k++) {
         	findSmallestTemperature.push(response.list[k].main.temp_min.toFixed(0));
         }
-         var j = findSmallestTemperature[0];
-		for (var i = 0; i < findSmallestTemperature.length; i++) {
-			if (j == findSmallestTemperature[i]) {
+        for (var k = 0; k < response.list.length; k++) {
+        	findLargetTemperature.push(response.list[k].main.temp_max.toFixed(0));
+        }
+         //join 2 arrays so you find the missing value on both high and low temperature
+        var children = findSmallestTemperature.concat(findLargetTemperature);
+
+        responseArray = sort_unique(children);
+
+        console.log("High and Low Temperatures without duplicates : "+responseArray) ;
+
+		var arr = [];
+		var a = responseArray;
+		var j = a[0];
+		for (var i = 0; i < a.length; i++) {
+			if (j == a[i]) {
 				j++;
 				continue;
 			}
@@ -185,9 +228,62 @@ function temperaturesNotOnHighLowdataset(response) {
 				j++;
 			}
 		}
-	}
+		console.log("Temperatures not in the data set  : "+arr);
+		return arr;
+	}	
+	function createDictionalHighLowTemperatures(response) {
+		var input=[];
+	    var result = {};
+	    var a = [], b = [], prev;
+	    var findSmallestTemperature = [];
+        for (var k = 0; k < response.list.length; k++) {
+        	findSmallestTemperature.push(response.list[k].main.temp_min.toFixed(0));
+        }
+        var findLargetTemperature = [];
+        for (var k = 0; k < response.list.length; k++) {
+        	findLargetTemperature.push(response.list[k].main.temp_max.toFixed(0));
+        }
+        //join 2 arrays so you find the missing value on both high and low temperature
+        var children = findSmallestTemperature.concat(findLargetTemperature);
+        var arr=children;
+        arr.sort();
+	    for ( var i = 0; i < arr.length; i++ ) {
+	        if ( arr[i] !== prev ) {
+	            a.push(arr[i]);
+	            b.push(1);
+	        } else {
+	            b[b.length-1]++;
+	        }
+	        prev = arr[i];
+	      }
+		 for (var i = 0; i < a.length; i++) {
+	         input.push({key:a[i], value:b[i]});
+	        }
 
-function sort_unique(sortTemperaturesRemoveDupl) {
+		 for(var i = 0; i < input.length; i++)
+			{
+			    result[input[i].key] = input[i].value;
+			}
+
+           console.log("dictionary based on the combined dataset :",result); // Just for testing
+           return result;
+       }
+
+       function displayOverallMinMax(response){
+       	var findSmallestTemperature = [];
+       	var findLargetTemperature = [];
+        for (var k = 0; k < response.list.length; k++) {
+        	findSmallestTemperature.push(response.list[k].main.temp_min.toFixed(0));
+        }
+        for (var k = 0; k < response.list.length; k++) {
+        	findLargetTemperature.push(response.list[k].main.temp_max.toFixed(0));
+        }
+         //join 2 arrays so you find the missing value on both high and low temperature
+        var maxAndMinArray = findSmallestTemperature.concat(findLargetTemperature); 
+        console.log("Overall Max and Min value in the dataset  : "+maxAndMinArray);
+        return maxAndMinArray;
+    }
+    function sort_unique(sortTemperaturesRemoveDupl) {
     if (sortTemperaturesRemoveDupl.length === 0) return sortTemperaturesRemoveDupl;
     sortTemperaturesRemoveDupl = sortTemperaturesRemoveDupl.sort(function (a, b) { return a*1 - b*1; });
     var ret = [sortTemperaturesRemoveDupl[0]];
